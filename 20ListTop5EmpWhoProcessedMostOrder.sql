9ac78acf31e5c079d15cfb1fcb3051325ca3ae58
payalsingh02 <59365910+payalsingh02@users.noreply.github.com>
2023-06-13 16:11:41 +0530

    Update 20ListTop5EmpWhoProcessedMostOrder.sql

---
+++ 20ListTop5EmpWhoProcessedMostOrder.sql
@@ -0,0 +1,6 @@
+SELECT employees.employee_id, CONCAT(employees.first_name, ' ', employees.last_name) AS employee_name, COUNT(orders.order_id) AS total_orders
+FROM employees
+INNER JOIN orders ON employees.employee_id = orders.employee_id
+GROUP BY employees.employee_id
+ORDER BY total_orders DESC
+LIMIT 5;
