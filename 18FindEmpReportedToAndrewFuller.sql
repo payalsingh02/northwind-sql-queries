f8096419bd9fd945917399fe67b4a656ba6320fd
payalsingh02 <59365910+payalsingh02@users.noreply.github.com>
2023-06-13 16:11:02 +0530

    Update 18FindEmpReportedToAndrewFuller.sql

---
+++ 18FindEmpReportedToAndrewFuller.sql
@@ -0,0 +1,4 @@
+SELECT employees.*
+FROM employees
+INNER JOIN employees AS manager ON employees.reports_to = manager.employee_id
+WHERE manager.last_name = 'Fuller' AND manager.first_name = 'Andrew';
