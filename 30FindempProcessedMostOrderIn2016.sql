02ec7555c943f735b8b98012e2474c599c3524f6
payalsingh02 <59365910+payalsingh02@users.noreply.github.com>
2023-06-13 16:14:11 +0530

    Update 30FindempProcessedMostOrderIn2016.sql

---
+++ 30FindempProcessedMostOrderIn2016.sql
@@ -0,0 +1,7 @@
+SELECT employees.*, COUNT(orders.order_id) AS total_orders
+FROM employees
+INNER JOIN orders ON employees.employee_id = orders.employee_id
+WHERE YEAR(orders.order_date) = 2016 AND MONTH(orders.order_date) = 8
+GROUP BY employees.employee_id
+ORDER BY total_orders DESC
+LIMIT 1;
