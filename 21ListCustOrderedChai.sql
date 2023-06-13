d866c912947e2856d38ea0c3e59e6a82748fa545
payalsingh02 <59365910+payalsingh02@users.noreply.github.com>
2023-06-13 16:11:54 +0530

    Update 21ListCustOrderedChai.sql

---
+++ 21ListCustOrderedChai.sql
@@ -0,0 +1,6 @@
+SELECT customers.*
+FROM customers
+INNER JOIN orders ON customers.customer_id = orders.customer_id
+INNER JOIN order_details ON orders.order_id = order_details.order_id
+INNER JOIN products ON order_details.product_id = products.product_id
+WHERE products.product_name = 'Chai';
