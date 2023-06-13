bcbe46a181119b9d8355cbe4e5ba5db832ed721b
payalsingh02 <59365910+payalsingh02@users.noreply.github.com>
2023-06-13 16:09:53 +0530

    Update 14FindMostExpensiveProduct.sql

---
+++ 14FindMostExpensiveProduct.sql
@@ -0,0 +1,8 @@
+SELECT categories.category_name, products.product_name, products.unit_price
+FROM categories
+INNER JOIN products ON categories.category_id = products.category_id
+WHERE products.unit_price = (
+  SELECT MAX(unit_price)
+  FROM products AS p
+  WHERE p.category_id = products.category_id
+);
