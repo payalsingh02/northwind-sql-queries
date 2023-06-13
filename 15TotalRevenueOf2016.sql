fc20b927a226c240aae6ed2d43ec159ff3cb1a9e
payalsingh02 <59365910+payalsingh02@users.noreply.github.com>
2023-06-13 16:10:13 +0530

    Update 15TotalRevenueOf2016.sql

---
+++ 15TotalRevenueOf2016.sql
@@ -0,0 +1,5 @@
+SELECT SUM(order_details.quantity * products.unit_price) AS total_revenue
+FROM order_details
+INNER JOIN products ON order_details.product_id = products.product_id
+INNER JOIN orders ON order_details.order_id = orders.order_id
+WHERE YEAR(orders.order_date) = 2016;
