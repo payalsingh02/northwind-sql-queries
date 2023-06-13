68a77b428da4cd58fe0acccc040476d87a12430b
payalsingh02 <59365910+payalsingh02@users.noreply.github.com>
2023-06-13 16:15:55 +0530

    Update 37.sql

---
+++ 37.sql
@@ -0,0 +1,9 @@
+SELECT products.product_id, products.product_name, SUM(order_details.quantity) AS total_quantity,
+  CASE
+    WHEN SUM(order_details.quantity) > 1000 THEN 'High Sales'
+    WHEN SUM(order_details.quantity) > 500 THEN 'Medium Sales'
+    ELSE 'Lower Sales'
+  END AS sales_category
+FROM products
+INNER JOIN order_details ON products.product_id = order_details.product_id
+GROUP BY products.product_id, products.product_name;
