7f463395dfc1486c389e9f9cae67cb20c544fd12
payalsingh02 <59365910+payalsingh02@users.noreply.github.com>
2023-06-13 16:07:22 +0530

    Update 5ListOfEmp&NoOfOrders.sql

---
+++ 5ListOfEmp&NoOfOrders.sql
@@ -0,0 +1,4 @@
+SELECT employees.employee_id, CONCAT(employees.first_name, ' ', employees.last_name) AS employee_name, COUNT(orders.order_id) AS total_orders
+FROM employees
+LEFT JOIN orders ON employees.employee_id = orders.employee_id
+GROUP BY employees.employee_id;
