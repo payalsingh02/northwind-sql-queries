fd3b97dfd2beebef8b3c3739fad0b87f26abce94
payalsingh02 <59365910+payalsingh02@users.noreply.github.com>
2023-06-13 16:13:12 +0530

    Update 26CusWhoOrderedMostChai.sql

---
+++ 26CusWhoOrderedMostChai.sql
@@ -0,0 +1,9 @@
+SELECT customers.*, SUM(order_details.quantity) AS total_chai_ordered
+FROM customers
+INNER JOIN orders ON customers.customer_id = orders.customer_id
+INNER JOIN order_details ON orders.order_id = order_details.order_id
+INNER JOIN products ON order_details.product_id = products.product_id
+WHERE products.product_name = 'Chai'
+GROUP BY customers.customer_id
+ORDER BY total_chai_ordered DESC
+LIMIT 1;
