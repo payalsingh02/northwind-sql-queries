9e32281621f0f8d1d1ddb4ed1330127c3025fe1d
payalsingh02 <59365910+payalsingh02@users.noreply.github.com>
2023-06-13 16:13:53 +0530

    Update 29FindMonthOf2016WithHighestSales.sql

---
+++ 29FindMonthOf2016WithHighestSales.sql
@@ -0,0 +1,8 @@
+SELECT MONTH(order_date) AS month, SUM(order_details.quantity * products.unit_price) AS total_sales
+FROM orders
+INNER JOIN order_details ON orders.order_id = order_details.order_id
+INNER JOIN products ON order_details.product_id = products.product_id
+WHERE YEAR(order_date) = 2016
+GROUP BY MONTH(order_date)
+ORDER BY total_sales DESC
+LIMIT 1;
