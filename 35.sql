ecfd36ade98457e802f52b05ee68e43cd19580af
payalsingh02 <59365910+payalsingh02@users.noreply.github.com>
2023-06-13 16:15:26 +0530

    Update 35.sql

---
+++ 35.sql
@@ -0,0 +1,5 @@
+SELECT YEAR(order_date) AS order_year, SUM(order_details.quantity * products.unit_price) AS total_sales
+FROM orders
+INNER JOIN order_details ON orders.order_id = order_details.order_id
+INNER JOIN products ON order_details.product_id = products.product_id
+GROUP BY YEAR(order_date);
