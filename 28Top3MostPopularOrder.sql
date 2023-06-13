90c9b571d92b5f6ade27c37959e1c8d8905680da
payalsingh02 <59365910+payalsingh02@users.noreply.github.com>
2023-06-13 16:13:37 +0530

    Update 28Top3MostPopularOrder.sql

---
+++ 28Top3MostPopularOrder.sql
@@ -0,0 +1,7 @@
+SELECT categories.category_name, COUNT(*) AS order_count
+FROM categories
+INNER JOIN products ON categories.category_id = products.category_id
+INNER JOIN order_details ON products.product_id = order_details.product_id
+GROUP BY categories.category_name
+ORDER BY order_count DESC
+LIMIT 3;
