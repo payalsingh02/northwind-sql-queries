25919c8b5f02130c048d54dd4411f84360857b00
payalsingh02 <59365910+payalsingh02@users.noreply.github.com>
2023-06-13 16:14:50 +0530

    Update 33.sql

---
+++ 33.sql
@@ -0,0 +1,7 @@
+SELECT suppliers.supplier_id, suppliers.company_name, COUNT(order_details.product_id) AS total_sold_products
+FROM suppliers
+INNER JOIN products ON suppliers.supplier_id = products.supplier_id
+INNER JOIN order_details ON products.product_id = order_details.product_id
+GROUP BY suppliers.supplier_id, suppliers.company_name
+ORDER BY total_sold_products DESC
+LIMIT 5;
