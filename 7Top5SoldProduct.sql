5f17005a297ce76ddffbfa09de5186aa635d6f1d
payalsingh02 <59365910+payalsingh02@users.noreply.github.com>
2023-06-13 16:08:00 +0530

    Update 7Top5SoldProduct.sql

---
+++ 7Top5SoldProduct.sql
@@ -0,0 +1,6 @@
+SELECT products.*, SUM(order_details.quantity) AS total_sold
+FROM products
+INNER JOIN order_details ON products.product_id = order_details.product_id
+GROUP BY products.product_id
+ORDER BY total_sold DESC
+LIMIT 5;
